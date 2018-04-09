5305a680309d37400b9a2d9a9c2beb51e0cf0b9b
Chris Nunez <cenunez47@gmail.com>
2018-04-09 16:02:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,8 +30,11 @@ const monologueLines = [
 
 const wordCountMap = monologueLines.reduce(function(newObj, mono) {
   let line = mono.split(' ').length;
+  
   if (!newObj[line]) {
     newObj[line] = 0
   }
+  
+  newObj[line]++
   
 }, {});
